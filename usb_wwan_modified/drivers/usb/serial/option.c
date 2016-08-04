a8c75b8c3b4bcf05f9d204a412041a9cf50e0583
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2016-08-04 15:10:51 +0530

    1.New build script for drivers/usb top level folder has been added. 2.Copyleft notices updated 3.print_buffer() in usb.h has been #ifdef-ed based on a build time flag to suppress the buffer bytes dump preferentially so that kern.log is not flooded. 4.Flag PRINT_BUFFER has to be defined with #define somewhere within KBuild makefiles or externally. 5..ko files rebuilt

--- usb_wwan_modified/drivers/usb/serial/option.c
+++ usb_wwan_modified/drivers/usb/serial/option.c
@@ -1,21 +1,20 @@
-/******************************************************************************************************
-* UMB - Universal Modified Bus Driver - simple USB driver for debugging
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
+/*************************************************************************************************************************
+#-------------------------------------------------------------------------------------------------------
+#NEURONRAIN USBMD - USB Debug Analytics Driver 
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #--------------------------------------------------------------------------------------------------------
-#Copyright (C):
-#Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+#Copyleft (Copyright+):
+#Srinivasan Kannan
+#(also known as: Shrinivaasan Kannan, Shrinivas Kannan)
 #Ph: 9791499106, 9003082186
 #Krishna iResearch Open Source Products Profiles:
 #http://sourceforge.net/users/ka_shrinivaasan,
@@ -24,8 +23,8 @@
 #Personal website(research): https://sites.google.com/site/kuja27/
 #emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com,
 #kashrinivaasan@live.com
-#--------------------------------------------------------------------------------------------------------
-********************************************************************************************************/
+#---------------------------------------------------------------------------------------------------------
+*************************************************************************************************************************/
 
 /*
   USB Driver for GSM modems
